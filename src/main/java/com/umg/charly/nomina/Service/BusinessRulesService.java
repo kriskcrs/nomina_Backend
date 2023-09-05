2af0b00cca3f94a2c2c6e84756260757b37984d8
ccaceres <ccaceres@bytesw.com>
2023-09-05 15:22:51 -0600

    se crean funciones de validacion para el password

--- src/main/java/com/umg/charly/nomina/Service/BusinessRulesService.java
+++ src/main/java/com/umg/charly/nomina/Service/BusinessRulesService.java
@@ -1,14 +1,34 @@
 package com.umg.charly.nomina.Service;
 
 
+import com.umg.charly.nomina.Entity.Company;
+import com.umg.charly.nomina.Repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1")
 @CrossOrigin
 public class BusinessRulesService {
+
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @GetMapping(path = "/bussinesRules")
+    private List<Company> rules(){
+      try {
+          return companyRepository.findAll();
+      }catch (Exception e){
+          System.out.println(e.getMessage());
+          return null;
+      }
+    }
+
 
 
 }
--- src/main/java/com/umg/charly/nomina/Service/UserService.java
+++ src/main/java/com/umg/charly/nomina/Service/UserService.java
@@ -2,6 +2,7 @@ package com.umg.charly.nomina.Service;
 
 
 import com.umg.charly.nomina.Entity.Company;
+import com.umg.charly.nomina.Entity.Option;
 import com.umg.charly.nomina.Entity.User;
 import com.umg.charly.nomina.Entity.UserRole;
 import com.umg.charly.nomina.Repository.CompanyRepository;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("v1")
@@ -53,18 +56,68 @@ public class UserService {
             User dataUser = userRepository.findByIdUser(user.getIdUser());
             if (dataUser != null) {
                 List<Company> dataCompany = companyRepository.findAll();
-               int plength = dataCompany.get(0).getPasswordlength();
-                if(user.getPassword().length() <= plength ){
+                int plength = dataCompany.get(0).getPasswordlength();
+                System.out.println(validateRules(user.getPassword()));
+                if (validateRules(user.getPassword())) {
                     String cript = new Encoding().crypt(user.getPassword());
                     dataUser.setPassword(cript);
                     userRepository.save(dataUser);
                     return "Exitoso";
                 }
-                return "Contraseña demasiado larga";
+                return "No cumple las condiciones";
             }
             return "Cliente no existe";
         }
         return "Parametros incorrectos";
+    }
+
+
+    private Boolean validateRules(String dataPass) {
+        if (dataPass != null) {
+            Optional<Company> dataCompany = companyRepository.findById(1L);
+            int mayusculasRequeridas = dataCompany.get().getPasswordAmountUppercase();
+            int minusculasRequeridas = dataCompany.get().getPasswordAmountLowercase();
+            int numerosRequeridos = dataCompany.get().getPasswordAmountNumber();
+            int simbolosRequeridos = dataCompany.get().getPasswordAmountSpecialCharacters();
+            int longitudMinima = dataCompany.get().getPasswordlength();
+            int longitudMaxima = 25;
+
+            if (dataPass.length() < longitudMinima || dataPass.length() > longitudMaxima) {
+                return false;
+            }
+            int mayusculasEncontradas = 0;
+            int minusculasEncontradas = 0;
+            int numerosEncontrados = 0;
+            int simbolosEncontrados = 0;
+
+            for (int i = 0; i < dataPass.length(); i++) {
+                char caracter = dataPass.charAt(i);
+                if (Character.isUpperCase(caracter)) {
+                    mayusculasEncontradas++;
+                } else if (Character.isLowerCase(caracter)) {
+                    minusculasEncontradas++;
+                } else if (Character.isDigit(caracter)) {
+                    numerosEncontrados++;
+                } else {
+                    simbolosEncontrados++;
+                }
+            }
+
+            if (mayusculasEncontradas >= mayusculasRequeridas) {
+                System.out.println("mayusculas -> OK " + mayusculasEncontradas);
+                if (minusculasEncontradas >= minusculasRequeridas) {
+                    System.out.println("minusculas -> OK "+minusculasEncontradas);
+                    if (numerosEncontrados >= numerosRequeridos) {
+                        System.out.println("numeros -> OK "+numerosEncontrados);
+                        if (simbolosEncontrados >= simbolosRequeridos) {
+                            System.out.println("simbolos -> OK "+simbolosEncontrados);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 
