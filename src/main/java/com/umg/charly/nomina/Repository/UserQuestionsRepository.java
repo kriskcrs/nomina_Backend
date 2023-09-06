5d7c1db7aeb7712deb18dac15870562030e83e56
ccaceres <ccaceres@bytesw.com>
2023-09-05 21:54:12 -0600

    cambios perdidos

--- src/main/java/com/umg/charly/nomina/Repository/UserQuestionsRepository.java
+++ src/main/java/com/umg/charly/nomina/Repository/UserQuestionsRepository.java
@@ -3,5 +3,8 @@ package com.umg.charly.nomina.Repository;
 import com.umg.charly.nomina.Entity.UserQuestions;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserQuestionsRepository extends JpaRepository<UserQuestions,Long> {
+    public List<UserQuestions> findByIdUser(String user);
 }
--- src/main/java/com/umg/charly/nomina/Service/BusinessRulesService.java
+++ src/main/java/com/umg/charly/nomina/Service/BusinessRulesService.java
@@ -1,15 +1,13 @@
 package com.umg.charly.nomina.Service;
 
 
-import com.umg.charly.nomina.Entity.Company;
-import com.umg.charly.nomina.Repository.CompanyRepository;
+import com.umg.charly.nomina.Entity.*;
+import com.umg.charly.nomina.Entity.Module;
+import com.umg.charly.nomina.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("v1")
@@ -18,17 +16,92 @@ public class BusinessRulesService {
 
     @Autowired
     CompanyRepository companyRepository;
+    @Autowired
+    UserQuestionsRepository userQuestionsRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    OptionRepository optionRepository;
+    @Autowired
+    MenuRepository menuRepository;
+    @Autowired
+    RoleOptionRepository roleOptionRepository;
+    @Autowired
+    ModuleRepository moduleRepository;
+
 
     @GetMapping(path = "/bussinesRules")
     private List<Company> rules(){
-      try {
           return companyRepository.findAll();
-      }catch (Exception e){
-          System.out.println(e.getMessage());
-          return null;
-      }
     }
 
+    @GetMapping(path = "/questionsUser/{user}")
+    private List<UserQuestions> userQuest(@PathVariable String user){
+        System.out.println(user);
+        if(!userQuestionsRepository.findByIdUser(user).isEmpty()){
+            return userQuestionsRepository.findByIdUser(user);
+        }
+        return null;
+    }
+
+    @GetMapping(path = "/role")
+    private List<Role> roleList(){
+        return roleRepository.findAll();
+    }
+
+    @GetMapping(path = "/option")
+    private List<Option> optionslist(){
+        return optionRepository.findAll();
+    }
+
+    @GetMapping(path = "/menu")
+    private List<Menu> menuList(){
+        return menuRepository.findAll();
+    }
+
+    @GetMapping(path = "/roleOption")
+    private List<RoleOption> roleOptionsList(){
+        return roleOptionRepository.findAll();
+    }
+
+    @GetMapping(path = "/module")
+    private List<Module> moduleList(){
+        return moduleRepository.findAll();
+    }
+
+
+    @PostMapping(path = "/bussinesRulesModify")
+    private String modify(@RequestBody Company company){
+        if(company.getIdcompany() != null){
+            if(company.getPasswordlength()>5){
+                companyRepository.save(company);
+                return "Se actualiza";
+            }
+            return "La contraseña minima debe ser mayor a 5 caracteres";
+        }
+        return "No se puede actualizar";
+    }
+
+
+    @PostMapping(path = "/createRol")
+    private Role createRole(@RequestBody Role role){
+        return roleRepository.save(role);
+    }
+
+    @PostMapping(path = "/createMenu")
+    private Menu createMenu(@RequestBody Menu menu){
+        return menuRepository.save(menu);
+    }
+
+    @PostMapping(path = "/createOption")
+    private Option createMenu(@RequestBody Option option){
+        return optionRepository.save(option);
+    }
+
+    @PostMapping(path = "/createModule")
+    private Module createMenu(@RequestBody Module module){
+        return moduleRepository.save(module);
+    }
 
 
 }
